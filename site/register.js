2b75ee422e6cb98d694d6b7db7f846829a7c51f6
thllq100 <328036144+thllq100@users.noreply.github.com>
2026-09-15 19:46:37 +0200

    Auflösung ZEW September

--- site/register.js
+++ site/register.js
@@ -1,6 +1,6 @@
 /* erzeugt von bauen.py – nicht von Hand ändern */
 window.THLLQ_REGISTER = {
- "erzeugt": "2026-09-15T19:42",
+ "erzeugt": "2026-09-15T19:46",
  "stand": {
   "n": 2,
   "aufgeloest": 2,
